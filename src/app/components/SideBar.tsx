f97fb21eaacdbae1dab9d06c8bf3efb528c3f7cb
Uksci123 <ukscihub123@gmail.com>
2023-10-08 20:24:06 +0530

    Google Authentication Setup -> SessionProvider Done

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NewChat from './NewChat'
 
 const SideBar = () => {
   return (
-    <div className='flex flex-col h-screen p-2'>SideBar</div>
+    <div className='flex flex-col h-screen p-2'>
+        <div className='flex-1'>
+            <div>
+                    {/* New Chat*/}
+                    <NewChat/>
+                <div>
+                    {/* Model Selection */}
+                </div>
+
+                    {/* Map through the Chat Row*/}
+            </div>
+        </div>
+    </div>
   )
 }
 
